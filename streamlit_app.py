11851d8b21fba1afe80c559ecf2189dda383c2b4
adamesa <122358452+adamesa@users.noreply.github.com>
2023-03-01 15:52:12 +0000

    Update streamlit_app.py
    insert values

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,6 @@ streamlit.dataframe(my_data_row)
 # allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+# will not work yet
+my_cur.execute("INSERT INTO pc_rivery_db.public.fruit_load_list values ('from streamlit')")
